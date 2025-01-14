be5bde11c033d960778727c09aa680e20d48deec
Kuba Szpilowski <jakub.szpilowski@gmail.com>
2025-01-14 10:25:24 +0100

    Gaussian and uniform mutation

--- source/genetic_algorithm/real/mutation.py
+++ source/genetic_algorithm/real/mutation.py
@@ -1,41 +1,64 @@
-import numpy
+import numpy as np
 import random
+import pygad
 
 
 class Mutation:
-
-    # TODO: Przykładowa implementacja do zmiany (możemy korzystać z atrybutów ga_instance)
     @staticmethod
-    def gaussian_mutation(offspring, ga_instance):
-
+    def gaussian_mutation(offspring : np.ndarray, ga_instance : pygad.GA) -> np.ndarray:
         """
-        Applies the random mutation which changes the values of a number of genes randomly.
-        The random value is selected either using the 'gene_space' parameter or the 2 parameters 'random_mutation_min_val' and 'random_mutation_max_val'.
-        It accepts a single parameter:
-            -offspring: The offspring to mutate.
-        It returns an array of the mutated offspring.
+        Mutate offspring with gaussian mutation
+
+        :param offspring: an array offspring to mutate.
+        :param ga_instance: instance of PyGAD.GA class
+        :return: an array of the mutated offspring.
         """
-
-        # If the mutation values are selected from the mutation space, the attribute 'gene_space' is not None. Otherwise, it is None.
-        # When the 'mutation_probability' parameter exists (i.e. not None), then it is used in the mutation. Otherwise, the 'mutation_num_genes' parameter is used.
-
         if ga_instance.mutation_probability is None:
-            # When the 'mutation_probability' parameter does not exist (i.e. None), then the parameter 'mutation_num_genes' is used in the mutation.
-            if not (ga_instance.gene_space is None):
-                # When the attribute 'gene_space' exists (i.e. not None), the mutation values are selected randomly from the space of values of each gene.
-                offspring = ga_instance.mutation_by_space(offspring)
-            else:
-                offspring = ga_instance.mutation_randomly(offspring)
-        else:
-            # When the 'mutation_probability' parameter exists (i.e. not None), then it is used in the mutation.
-            if not (ga_instance.gene_space is None):
-                # When the attribute 'gene_space' does not exist (i.e. None), the mutation values are selected randomly based on the continuous range specified by the 2 attributes 'random_mutation_min_val' and 'random_mutation_max_val'.
-                offspring = ga_instance.mutation_probs_by_space(offspring)
-            else:
-                offspring = ga_instance.mutation_probs_randomly(offspring)
+            return offspring
+
+        mutation_probability = ga_instance.mutation_probability
+        a = ga_instance.init_range_low
+        b = ga_instance.init_range_high
+
+        gene_idx = np.random.choice(range(offspring.shape[1]))
+        for chromosome_index in range(offspring.shape[0]):
+            if random.random() <= mutation_probability:
+                n = np.random.normal(0, 1)
+                offspring[chromosome_index, gene_idx] += n
+
+                if ga_instance.gene_space is not None:
+                    if isinstance(ga_instance.gene_space, list):
+                        gene_space = ga_instance.gene_space[gene_idx]
+                    else:
+                        gene_space = ga_instance.gene_space
+
+                    if isinstance(gene_space, dict) and "low" in gene_space and "high" in gene_space:
+                        a = gene_space["low"]
+                        b = gene_space["high"]
+
+                offspring[chromosome_index, gene_idx] = np.clip(offspring[chromosome_index, gene_idx], a, b)
 
         return offspring
 
+
     @staticmethod
     def uniform_mutation(offspring, ga_instance):
-        pass
+        """
+        Mutate offspring with uniform mutation
+        :param offspring: array of the offspring to be mutated.
+        :param ga_instance: instance of PyGAD.GA class
+        :return: array of the mutated offspring.
+        """
+        if ga_instance.mutation_probability is None:
+            return offspring
+
+        mutation_probability = ga_instance.mutation_probability
+        a = ga_instance.init_range_low
+        b = ga_instance.init_range_high
+
+        gene_idx = np.random.choice(range(offspring.shape[1]))
+        for chromosome_index in range(offspring.shape[0]):
+            if random.random() <= mutation_probability:
+                offspring[chromosome_index, gene_idx] = random.uniform(a, b)
+
+        return offspring
